ac7b961ef8b9cf9f50cd420a2b85ebc893c2a543
Dilnavas CP <dilnavasdilu07@gmail.com>
2021-10-26 12:06:07 +0530

    update info route

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.get('/', (req, res) => {
 
 app.get('/info', (req, res) => {
   res.json({
+    project: 'node js',
     host: 'heroku',
     deployment: 'ci/cd using github actions',
   })
@@ -19,16 +20,15 @@ app.get('/info', (req, res) => {
 
 app.get('/hello', (req, res) => {
   res.json({
-    message: "Hellow guys hehehe .............."
+    message: 'Hellow guys hehehe ..............',
   })
 })
 
 app.get('/test', (req, res) => {
   res.json({
-    message: "This is a test route"
+    message: 'This is a test route',
   })
 })
-
 
 app.listen(process.env.PORT || 5000, () =>
   console.log('server started at port 5000')
